5ba9058ae6937d35ebd0a350e3f6ba270e1d43a5
cloud_user <yogen48@hotmail.com>
2024-03-15 22:45:51 +0100

    Parallel Builds

--- terraform/codebuild/main.tf
+++ terraform/codebuild/main.tf
@@ -5,7 +5,7 @@ locals {
     "simple-pong"
   ]
   repository_name = "simple-mono-repo"
-  repository_url   = "730335205732.dkr.ecr.us-east-1.amazonaws.com/simple-ping"
+#   repository_url   = "730335205732.dkr.ecr.us-east-1.amazonaws.com/simple-ping"
   build_image      = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
 }
 
@@ -38,7 +38,7 @@ resource "aws_codebuild_project" "this" {
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = file("${path.module}/buildspec.yaml")
+    # buildspec = file("${path.module}/buildspec.yaml")
   }
 
   logs_config {
